583bf04aaf7c836b9075f7e4065b478233a7a994
cocomatt <matt.currie@outlook.com>
2018-10-26 13:32:41 +0000

    installed eslint package

--- lookup_people.js
+++ lookup_people.js
@@ -1,13 +1,15 @@
-const pg = require("pg");
-const settings = require("./settings"); // settings.json
+'use strict';
+
+const pg = require('pg');
+const settings = require('./settings'); // settings.json
 
 const client = new pg.Client({
-  user : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host : settings.hostname,
-  port : settings.port,
-  ssl : settings.ssl
+  user: settings.user,
+  password: settings.password,
+  database: settings.database,
+  host: settings.hostname,
+  port: settings.port,
+  ssl: settings.ssl,
 });
 
 const arg = process.argv[2];
@@ -19,15 +21,11 @@ function queryByName(name) {
 
 function displayQueryByName(results) {
   results.forEach(function(val, index) {
-    // console.log('val, ', val);
-    // console.log('index, ', index);
-    // console.log('results, ', results);
-    // console.log('results[index]: ', results[index]);
     const id = results[index].id;
     const firstName = results[index].first_name;
     const lastName = results[index].last_name;
-    const dateOfBirth = results[index].birthdate.toISOString().slice(0,10);
-     console.log(`- ${id}: ${firstName} ${lastName}, born '${dateOfBirth}'`);
+    const dateOfBirth = results[index].birthdate.toISOString().slice(0, 10);
+    console.log(`- ${id}: ${firstName} ${lastName}, born '${dateOfBirth}'`);
   });
 };
 
@@ -36,17 +34,15 @@ client.connect((err) => {
     return console.error('Connection Error', err);
   }
   client.query(queryByName(arg), (err, result) => {
-    // console.log(`arg: ${arg}, result: ${result}`);
     if (err) {
       return console.error('error running query', err);
     }
     console.log('Searching ...');
-    console.log(`Found ${result.rowCount} person(s) by the name '${arg}':`);
-    if (result.rowCount > 0) {
-      // console.log('result.rows: ', result.rows);
-      displayQueryByName(result.rows);
+    if (result.rowCount === 0) {
+      console.log('There are no matches');
     } else {
-      console.log('');
+      console.log(`Found ${result.rowCount} person(s) by the name '${arg}':`);
+      displayQueryByName(result.rows);
     }
     client.end();
   });
